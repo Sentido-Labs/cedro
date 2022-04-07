549bdd2308700182f4d46ef772608781e033202a
Alberto González Palomo <bugs@sentido-labs.com>
2022-04-07 17:52:51 +0200

    Make foreach-struct-print example complete for compilation.

--- test/foreach-struct-print.c
+++ test/foreach-struct-print.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 #pragma Cedro 1.0
 void imprime_double(double n, FILE* salida)
 {
@@ -7,7 +11,11 @@ void imprime_double(double n, FILE* salida)
 typedef uint32_t Color_ARGB;
 void imprime_Color_ARGB(Color_ARGB c, FILE* salida)
 {
-  fprintf(salida, "#%.6X", c);
+  if ((c & 0xFF000000) == 0xFF000000) {
+    fprintf(salida, "#%.6X", c & 0x00FFFFFF);
+  } else {
+    fprintf(salida, "#%.8X", c);
+  }
 }
 
 #foreach { CAMPOS {{                            \
@@ -28,3 +36,9 @@ void imprime_Punto(Punto punto, FILE* salida)
 #foreach }
 }
 #foreach }
+
+int main(void)
+{
+  Punto punto = { .x = 12.3, .y = 4.56, .color = 0xFFa3f193 };
+  imprime_Punto(punto, stderr);
+}
--- test/reference/foreach-struct-print.c
+++ test/reference/foreach-struct-print.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 void imprime_double(double n, FILE* salida)
 {
   fprintf(salida, "%f", n);
@@ -6,7 +10,11 @@ void imprime_double(double n, FILE* salida)
 typedef uint32_t Color_ARGB;
 void imprime_Color_ARGB(Color_ARGB c, FILE* salida)
 {
-  fprintf(salida, "#%.6X", c);
+  if ((c & 0xFF000000) == 0xFF000000) {
+    fprintf(salida, "#%.6X", c & 0x00FFFFFF);
+  } else {
+    fprintf(salida, "#%.8X", c);
+  }
 }
 
 typedef struct Punto {
@@ -20,4 +28,10 @@ void imprime_Punto(Punto punto, FILE* salida)
   imprime_double(punto.x, salida); putc('\n', salida);
   imprime_double(punto.y, salida); putc('\n', salida);
   imprime_Color_ARGB(punto.color, salida); putc('\n', salida);
+}
+
+int main(void)
+{
+  Punto punto = { .x = 12.3, .y = 4.56, .color = 0xFFa3f193 };
+  imprime_Punto(punto, stderr);
 }
